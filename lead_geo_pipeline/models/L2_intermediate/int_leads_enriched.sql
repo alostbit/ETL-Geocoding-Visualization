3fcd7e30565a1e1a522c6e437a1a1620b6e95c58
alostbit <58672565+alostbit@users.noreply.github.com>
2026-01-04 22:11:47 -0600

    generated L1, L2, L3 models and schema tests

---
+++ lead_geo_pipeline/models/L2_intermediate/int_leads_enriched.sql
@@ -0,0 +1,36 @@
+WITH leads AS (
+    SELECT * FROM {{ ref('stg_leads') }}
+),
+
+geo AS (
+    SELECT * FROM {{ ref('stg_zip_reference') }}
+),
+
+enriched AS (
+    SELECT
+        l.lead_id,
+        l.company_name,
+        l.zip_code,
+        g.city,
+        g.state,
+        g.county,
+        g.latitude,
+        g.longitude,
+        -- Create Snowflake GEOGRAPHY point
+        ST_MAKEPOINT(g.longitude, g.latitude) AS location_geo,
+        l.lead_source,
+        l.lead_status,
+        l.industry,
+        l.company_size,
+        l.deal_value,
+        l.sales_rep,
+        l.created_date,
+        l.close_date,
+        -- Calculated fields
+        DATEDIFF(day, l.created_date, CURRENT_DATE()) AS lead_age_days,
+        DATEDIFF(day, l.created_date, l.close_date) AS days_to_close
+    FROM leads l
+    LEFT JOIN geo g ON l.zip_code = g.zip_code
+)
+
+SELECT * FROM enriched
